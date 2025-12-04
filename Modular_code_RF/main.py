d480e3e09fd834976634547a1fc72279bd742b34
Ananth S <83899514+Ananth09@users.noreply.github.com>
2025-12-04 23:05:38 +0530

    Comment out uvicorn run block in main.py
    Comment out the main execution block for uvicorn.

--- Modular_code_RF/main.py
+++ Modular_code_RF/main.py
@@ -184,6 +184,6 @@ def predict_csv(payload: PredictionInput):
         headers={"Content-Disposition": "attachment; filename=prediction.csv"}
     )
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
